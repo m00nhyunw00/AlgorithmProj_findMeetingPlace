25d9ee1f75de921d74a63de172842b1d05bee4f3
Hyunwoo Moon <113423186+m00nhyunw00@users.noreply.github.com>
2023-12-16 16:49:17 +0900

    Refactor: 디렉토리 구조 변경

--- src/subway_graph.py
+++ src/subway_graph.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-from queue import Queue
 from matplotlib import rc
 
 font = 'AppleGothic'
@@ -126,44 +125,3 @@ def connect_stations(object, line, line_num):
 
 def add_weight_to_edge(object, station1, station2, weight):
     object.graph[station1][station2]['weight'] = weight
-
-def bfs_search(object, start_station, end_station):
-    if start_station not in object.graph.nodes() or end_station not in object.graph.nodes():
-        print("출발역 또는 도착역이 그래프에 존재하지 않습니다.")
-        return
-
-    visited = set()
-    queue = Queue()
-    path = {}
-
-    queue.put(start_station)
-    visited.add(start_station)
-
-    while not queue.empty():
-        current_station = queue.get()
-
-        for neighbor in object.graph.neighbors(current_station):
-            if neighbor not in visited:
-                queue.put(neighbor)
-                visited.add(neighbor)
-                path[neighbor] = current_station
-
-                if neighbor == end_station:
-                    # 경로가 찾아졌으면 탐색 종료
-                    queue.queue.clear()
-                    break
-
-    if end_station not in path:
-        print("경로가 존재하지 않습니다.")
-        return
-
-    # 경로 출력
-    current = end_station
-    path_list = [current]
-
-    while current != start_station:
-        current = path[current]
-        path_list.append(current)
-
-    path_list.reverse()
-    print("경로:", path_list)

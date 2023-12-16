bda53b16ed7dccfb63207132cafd1d6910744592
Hyunwoo Moon <113423186+m00nhyunw00@users.noreply.github.com>
2023-12-16 19:27:34 +0900

    Update: 가중치 구현

--- src/main.py
+++ src/main.py
@@ -8,39 +8,75 @@ def main():
     subway = subway_graph.SubwayGraph()
 
     line1 = [station_info.line1, station_info.line1_1, station_info.line1_2, station_info.line1_3]
+    line1_time = [station_info.line1_time, station_info.line1_1_time, station_info.line1_2_time, station_info.line1_3_time]
+
     line2 = [station_info.line2, station_info.line2_1, station_info.line2_2]
+    line2_time = [station_info.line2_time, station_info.line2_1_time, station_info.line2_2_time]
+
     line3 = [station_info.line3]
+    line3_time = [station_info.line3_time]
+
     line4 = [station_info.line4]
+    line4_time = [station_info.line4_time]
+
     line5 = [station_info.line5, station_info.line5_1]
-    line6 = [station_info.line6]
+    line5_time = [station_info.line5_time, station_info.line5_1_time]
+
+    line6 = [station_info.line6, station_info.line6_1]
+    line6_time = [station_info.line6_time, station_info.line6_1_time]
+
     line7 = [station_info.line7]
+    line7_time = [station_info.line7_time]
+
     line8 = [station_info.line8]
+    line8_time = [station_info.line8_time]
+
     line9 = [station_info.line9]
+    line9_time = [station_info.line9_time]
+
     lineSuinBundang = [station_info.lineSuinBundang]
+    lineSuinBundang_time = [station_info.lineSuinBundang_time]
+
     lineSinbundang = [station_info.lineSinbundang]
+    lineSinbundang_time = [station_info.lineSinbundang_time]
+
     lineGyeongui = [station_info.lineGyeongui, station_info.lineGyeongui_1]
+    lineGyeongui_time = [station_info.lineGyeongui_time, station_info.lineGyeongui_1_time]
+
     lineSinlim = [station_info.lineSinlim]
+    lineSinlim_time = [station_info.lineSinlim_time]
+
     lineUii = [station_info.lineUii]
+    lineUii_time = [station_info.lineUii_time]
+
     lineAirportRailroad = [station_info.lineAirportRailroad]
+    lineAirportRailroad_time = [station_info.lineAirportRailroad_time]
 
-    lines = [line1, line2, line3, line4, line5, line6, line7, line8, line9, lineSuinBundang, lineSinbundang, lineGyeongui, lineSinlim, lineUii, lineAirportRailroad]
+    lines_name = ['line1', 'line2', 'line3', 'line4', 'line5', 'line6', 'line7', 'line8', 'line9', 'lineSuinBundang', 'lineSinbundang'
+             , 'lineGyeongui', 'lineSinlim', 'lineUii', 'lineAirportRailroad']
 
-    lines_name = ['line1', 'line2', 'line3', 'line4', 'line5', 'line6', 'line7', 'line8', 'line9', 'lineSuinBundang',
-             'lineSinbundang', 'lineGyeongui', 'lineSinlim', 'lineUii', 'lineAirportRailroad']
+    lines = [line1, line2, line3, line4, line5, line6, line7, line8, line9, lineSuinBundang, lineSinbundang
+             , lineGyeongui, lineSinlim, lineUii, lineAirportRailroad]
+
+    lines_times = [line1_time, line2_time, line3_time, line4_time, line5_time, line6_time, line7_time, line8_time, line9_time, lineSuinBundang_time, lineSinbundang_time
+             , lineGyeongui_time, lineSinlim_time, lineUii_time, lineAirportRailroad_time]
 
     for line, name in zip(lines, lines_name):
         subway_graph.make_stations(subway, line)  # 역간 간선 생성 및 연결
         subway_graph.connect_stations(subway, line, name)   # 지하철 노드 생성
 
-    # subway_graph.add_weight_to_edge(subway, '충무로', '동대입구', 2)
-    # subway_graph.add_weight_to_edge(subway, '동대입구', '약수', 3)
-    # subway_graph.add_weight_to_edge(subway, '약수', '금호', 1)
-    # subway_graph.add_weight_to_edge(subway, '금호', '옥수', 4)
-    # subway_graph.add_weight_to_edge(subway, '옥수', '압구정', 2)
+    for line, weights in zip(lines, lines_times):
+        for station, weight in zip(line, weights):
+            for i in range(len(weight) - 1):
+                station1 = str(station[i])
+                station2 = str(station[i + 1])
+                subway_graph.add_weight_to_edge(subway, station1, station2, int(weight[i]))
+
 
     # 출발지와 목적지 입력 받기
     start_station = input("출발역을 입력하세요: ")
     end_station = input("도착역을 입력하세요: ")
+    print('\n')
 
     # BFS 경로 탐색
     bfs.bfs_search(subway, start_station, end_station)
--- src/test/test.py
+++ src/test/test.py
@@ -8,8 +8,8 @@ def compare_num_stations_times(name, stations, times):
     else:
         print("X\n")
 
-# station_info.line2.reverse()
-# print(station_info.line2)
+# station_info.line9_time.reverse()
+# print(station_info.line9_time)
 
 compare_num_stations_times("1", station_info.line1_1, station_info.line1_1_time)
 
@@ -30,6 +30,26 @@ compare_num_stations_times("3", station_info.line3, station_info.line3_time)
 compare_num_stations_times("4", station_info.line4, station_info.line4_time)
 
 compare_num_stations_times("5", station_info.line5, station_info.line5_time)
+
+compare_num_stations_times("6", station_info.line6, station_info.line6_time)
+
+compare_num_stations_times("6_1", station_info.line6_1, station_info.line6_1_time)
+
+compare_num_stations_times("SuinBundang", station_info.lineSuinBundang, station_info.lineSuinBundang_time)
+
+compare_num_stations_times("Sinbundang", station_info.lineSinbundang, station_info.lineSinbundang_time)
+
+compare_num_stations_times("Gyeongui", station_info.lineGyeongui, station_info.lineGyeongui_time)
+
+compare_num_stations_times("Gyeongui_1", station_info.lineGyeongui_1, station_info.lineGyeongui_1_time)
+
+compare_num_stations_times("Sinlim", station_info.lineSinlim, station_info.lineSinlim_time)
+
+compare_num_stations_times("Uii", station_info.lineUii, station_info.lineUii_time)
+
+compare_num_stations_times("AirportRailroad", station_info.lineAirportRailroad, station_info.lineAirportRailroad_time)
+
+
 
 
 

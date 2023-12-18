f1a569cf6b5746aa5b59a4b9388ca94b8a11d12d
Hyunwoo Moon <113423186+m00nhyunw00@users.noreply.github.com>
2023-12-19 07:12:39 +0900

    Refactor: main.py 모듈화 및 구조 변경

--- src/main.py
+++ src/main.py
@@ -6,28 +6,55 @@ from dijk import dijk
 from astar import astar_1, astar_2
 import numpy as np
 
+# 지하철 관련 정보 station_info.py에서 가져옴
+line1 = [station_info.line1, station_info.line1_1, station_info.line1_2, station_info.line1_3]
+line1_time = [station_info.line1_time, station_info.line1_1_time, station_info.line1_2_time, station_info.line1_3_time]
 
-def main():
-    subway = subway_graph.SubwayGraph()  # 지하철 노선도 그래프 생성
-    subway.visualize()  # 지하철 노선도 그래프 시각화
+line2 = [station_info.line2, station_info.line2_1, station_info.line2_2]
+line2_time = [station_info.line2_time, station_info.line2_1_time, station_info.line2_2_time]
 
-    for line, name in zip(lines, lines_name):
-        subway_graph.make_stations(subway, line)  # 역간 간선 생성 및 연결
-        subway_graph.connect_stations(subway, line, name)  # 지하철 노드 생성
+line3 = [station_info.line3]
+line3_time = [station_info.line3_time]
 
-    for line, weights in zip(lines, lines_times):
-        for station, weight in zip(line, weights):
-            for i in range(len(weight) - 1):
-                station1 = str(station[i])
-                station2 = str(station[i + 1])
-                subway_graph.add_weight_to_edge(subway, station1, station2, int(weight[i]))  # 간선 가중치 설정
+line4 = [station_info.line4]
+line4_time = [station_info.line4_time]
 
-    recommed_process()  # 추천 프로세스 실행
+line5 = [station_info.line5, station_info.line5_1]
+line5_time = [station_info.line5_time, station_info.line5_1_time]
 
+line6 = [station_info.line6, station_info.line6_1]
+line6_time = [station_info.line6_time, station_info.line6_1_time]
 
-if __name__ == "__main__":
-    main()
+line7 = [station_info.line7]
+line7_time = [station_info.line7_time]
 
+line8 = [station_info.line8]
+line8_time = [station_info.line8_time]
+
+line9 = [station_info.line9]
+line9_time = [station_info.line9_time]
+
+lineSuinBundang = [station_info.lineSuinBundang]
+lineSuinBundang_time = [station_info.lineSuinBundang_time]
+
+lineSinbundang = [station_info.lineSinbundang]
+lineSinbundang_time = [station_info.lineSinbundang_time]
+
+lineGyeongui = [station_info.lineGyeongui, station_info.lineGyeongui_1]
+lineGyeongui_time = [station_info.lineGyeongui_time, station_info.lineGyeongui_1_time]
+
+lineSinlim = [station_info.lineSinlim]
+lineSinlim_time = [station_info.lineSinlim_time]
+
+lineUii = [station_info.lineUii]
+lineUii_time = [station_info.lineUii_time]
+
+lines = [line1, line2, line3, line4, line5, line6, line7, line8, line9, lineSuinBundang, lineSinbundang
+    , lineGyeongui, lineSinlim, lineUii]
+
+lines_times = [line1_time, line2_time, line3_time, line4_time, line5_time, line6_time, line7_time, line8_time,
+               line9_time, lineSuinBundang_time, lineSinbundang_time
+    , lineGyeongui_time, lineSinlim_time, lineUii_time]
 
 class User:
     def __init__(self, name):
@@ -133,57 +160,28 @@ def recommend_algorithm(object, users, meeting_places, algorithm):
 
     return result, entire_time, paths
 
+def main():
+    subway = subway_graph.SubwayGraph()  # 지하철 노선도 그래프 생성
+    subway.visualize()  # 지하철 노선도 그래프 시각화
 
-# 지하철 관련 정보를 station_info.py에서 가져옴
-line1 = [station_info.line1, station_info.line1_1, station_info.line1_2, station_info.line1_3]
-line1_time = [station_info.line1_time, station_info.line1_1_time, station_info.line1_2_time, station_info.line1_3_time]
+    for line, name in zip(lines, station_info.line_class_name):
+        subway_graph.make_stations(subway, line)  # 역간 간선 생성 및 연결
+        subway_graph.connect_stations(subway, line, name)  # 지하철 노드 생성
 
-line2 = [station_info.line2, station_info.line2_1, station_info.line2_2]
-line2_time = [station_info.line2_time, station_info.line2_1_time, station_info.line2_2_time]
+    for line, weights in zip(lines, lines_times):
+        for station, weight in zip(line, weights):
+            for i in range(len(weight) - 1):
+                station1 = str(station[i])
+                station2 = str(station[i + 1])
+                subway_graph.add_weight_to_edge(subway, station1, station2, int(weight[i]))  # 간선 가중치 설정
 
-line3 = [station_info.line3]
-line3_time = [station_info.line3_time]
+    recommed_process(subway)  # 추천 프로세스 실행
 
-line4 = [station_info.line4]
-line4_time = [station_info.line4_time]
 
-line5 = [station_info.line5, station_info.line5_1]
-line5_time = [station_info.line5_time, station_info.line5_1_time]
+if __name__ == "__main__":
+    main()
 
-line6 = [station_info.line6, station_info.line6_1]
-line6_time = [station_info.line6_time, station_info.line6_1_time]
 
-line7 = [station_info.line7]
-line7_time = [station_info.line7_time]
 
-line8 = [station_info.line8]
-line8_time = [station_info.line8_time]
 
-line9 = [station_info.line9]
-line9_time = [station_info.line9_time]
 
-lineSuinBundang = [station_info.lineSuinBundang]
-lineSuinBundang_time = [station_info.lineSuinBundang_time]
-
-lineSinbundang = [station_info.lineSinbundang]
-lineSinbundang_time = [station_info.lineSinbundang_time]
-
-lineGyeongui = [station_info.lineGyeongui, station_info.lineGyeongui_1]
-lineGyeongui_time = [station_info.lineGyeongui_time, station_info.lineGyeongui_1_time]
-
-lineSinlim = [station_info.lineSinlim]
-lineSinlim_time = [station_info.lineSinlim_time]
-
-lineUii = [station_info.lineUii]
-lineUii_time = [station_info.lineUii_time]
-
-lines_name = ['line1', 'line2', 'line3', 'line4', 'line5', 'line6', 'line7', 'line8', 'line9', 'lineSuinBundang',
-              'lineSinbundang'
-    , 'lineGyeongui', 'lineSinlim', 'lineUii']
-
-lines = [line1, line2, line3, line4, line5, line6, line7, line8, line9, lineSuinBundang, lineSinbundang
-    , lineGyeongui, lineSinlim, lineUii]
-
-lines_times = [line1_time, line2_time, line3_time, line4_time, line5_time, line6_time, line7_time, line8_time,
-               line9_time, lineSuinBundang_time, lineSinbundang_time
-    , lineGyeongui_time, lineSinlim_time, lineUii_time]
